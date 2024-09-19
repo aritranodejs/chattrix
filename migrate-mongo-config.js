9172902dec825bbfbf75bb3c4399f0e0541f8a6e
aritranodejs <aritra.nodejsdeveloper@gmail.com>
2024-09-19 13:33:35 +0530

    Update migrate-mongo-config.js

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -3,7 +3,7 @@
 const config = {
   mongodb: {
     // TODO Change (or review) the url to your MongoDB:
-    url: "mongodb+srv://aritranodejsdeveloper:D1nFfhIQ5hf9TABW@cluster0.v5wk1hp.mongodb.net?retryWrites=true&w=majority&appName=Cluster0",
+    url: "mongodb+srv://aritranodejsdeveloper:1ElGvZlwvqX80PVN@cluster0.v5wk1hp.mongodb.net?retryWrites=true&w=majority&appName=Cluster0",
 
     // TODO Change this to your database name:
     databaseName: "chattrix",
